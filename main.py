7ce826124e320d1f502d5004177e26676721cf13
buketcoban <100703834+buketcoban@users.noreply.github.com>
2025-08-01 01:20:18 +0300

    Update main.py
    Yorum satırları düzenlendi.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import boto3
 from botocore.client import Config
 import uuid
 
-# LocalStack endpoint
 s3 = boto3.client(
     "s3",
     endpoint_url="http://localhost:4566",
@@ -16,24 +15,19 @@ bucket_name = "demo-bucket"
 file_name = f"{uuid.uuid4()}.txt"
 file_content = "Merhaba S3, bu dosya localstack içindir."
 
-# Bucket oluştur (varsa hata vermez)
 s3.create_bucket(Bucket=bucket_name)
 
-# Dosya oluştur
 with open(file_name, "w") as f:
     f.write(file_content)
 
-# Upload et
 s3.upload_file(file_name, bucket_name, file_name)
-print(f"✅ Yüklendi: {file_name}")
+print(f"Yüklendi: {file_name}")
 
-# Listele
-print("📂 Bucket içeriği:")
+print("Bucket içeriği:")
 objects = s3.list_objects_v2(Bucket=bucket_name)
 for obj in objects.get("Contents", []):
     print(f" - {obj['Key']}")
 
-# Dosyayı sil
 s3.delete_object(Bucket=bucket_name, Key=file_name)
-print(f"❌ Silindi: {file_name}")
+print(f"Silindi: {file_name}")
 
